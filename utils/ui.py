56a55602ce8dcba9d4cc12efc410ddbc806a4392
idfk <204621528+hackyue@users.noreply.github.com>
2026-02-10 20:08:00 -0500

    Version Changes
    Version Changes

--- utils/ui.py
+++ utils/ui.py
@@ -566,7 +566,7 @@ class AccountManagerUI:
     def __init__(self, root, manager):
         self.root = root
         self.manager = manager
-        self.APP_VERSION = "2.4.0"
+        self.APP_VERSION = "2.4.1"
         self._game_name_after_id = None
         self._game_name_label_after_id = None
         self._game_name_request_token = 0
@@ -584,7 +584,7 @@ class AccountManagerUI:
             except:
                 pass
         
-        self.root.title("FRAM v2.4.0 - made by evanovar - modified by hackyue")
+        self.root.title("FRAM v2.4.1 - made by evanovar - modified by hackyue")
         self.root.geometry("600x600")
         self.root.configure(bg="#2b2b2b")
         self.root.resizable(True, True)
